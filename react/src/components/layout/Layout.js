e35cdf0702b80d9a8ed0e9686e005a7bb933e95f
Jon <jon.howell314@gmail.com>
2022-03-28 20:29:10 +0800

    basic layout routing

--- react/src/components/layout/Layout.js
+++ react/src/components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './Layout.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import TodoPage from '../pages/todo/TodoPage';
 import TicTacToePage from '../pages/tic_tac_toe/TicTacToePage';
 
@@ -8,12 +9,17 @@ class Layout extends React.Component {
   render() {
     return (
       <div className="layout">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<TodoPage />} />
-            <Route path="/tic_tac_toe" element={<TicTacToePage />} />
-          </Routes>
-        </BrowserRouter>
+        <div className="nav-bar"></div>
+
+        <div className="page-wrapper">
+          <BrowserRouter className="page">
+            <Routes>
+              <Route path="/todo" element={<TodoPage />} />
+              <Route path="/tic_tac_toe" element={<TicTacToePage />} />
+              <Route path="*" element={<Navigate to="/todo" replace />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
       </div>
     );
   }

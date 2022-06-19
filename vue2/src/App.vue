e21de7136b07ffd926a674af8f72db4216b74d3c
Jon Howell <jon.howell314@gmail.com>
2022-06-19 20:39:10 +0800

    rough layout

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style>
-#app {
+.ember-application {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
